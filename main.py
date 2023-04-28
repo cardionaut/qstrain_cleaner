573e23b7b2a5ed40bc9c8182e9f6e3bf68a96c5e
Sebastian Balzer <sebastian.balzer@gmx.ch>
2023-04-28 15:38:46 +0200

    added name split function + pdf reader

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ import hydra
 import pandas as pd
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
-from pathlib import Path
 
 from utils.cleaner import Cleaner
 
 @hydra.main(version_base=None, config_path='.', config_name='config')
 def main(config: DictConfig) -> None:
     root = config.root_dir
-    out_file = config.out_file
+    out_path = config.out_path
 
-    cleaner = Cleaner(root, out_file)
+    cleaner = Cleaner(root, out_path)
     new_data = cleaner()
 
-    with pd.ExcelWriter(out_file, datetime_format='DD/MM/YYYY') as writer:
+    with pd.ExcelWriter(os.path.join(root, 'ScreeningListe.xlsx'), datetime_format='DD/MM/YYYY') as writer:
         new_data.to_excel(writer, index=False)
 
 if __name__ == '__main__':
--- utils/cleaner.py
+++ utils/cleaner.py
@@ -1,13 +1,63 @@
-from typing import Any
+import os
+import pypdf
+import re
+
 import pandas as pd
 import numpy as np
 from loguru import logger
+from pathlib import Path
 
 
 class Cleaner:
-    def __init__(self, root, out_file) -> None:
+    def __init__(self, root, out_path) -> None:
         self.root = root
-        self.out_file = out_file
+        self.patients = [f.name for f in os.scandir(self.root) if f.is_dir()]
+        self.out_file = pd.read_excel(out_path, sheet_name='SPSS Export (2)')
 
     def __call__(self) -> pd.DataFrame:
-        pass
+        for patient in self.patients:
+            first_name, last_name = self.split_name(patient)
+            logger.debug(f'{patient}: {first_name} _ {last_name}')
+            self.read_pdf(patient, first_name, last_name)
+
+        return self.out_file
+
+    def split_name(self, patient):
+        regex = re.compile(u'[^a-zA-Z\'-] + ')
+        cleaned_patient = regex.sub('', patient)
+        name_list = cleaned_patient.split(sep=' ')
+
+        if len(name_list) == 2:
+            first_name = name_list[1]
+            last_name = name_list[0]
+        elif (
+            'von' in [name.lower() for name in name_list]
+            or 'van' in [name.lower() for name in name_list]
+            or 'le' in [name.lower() for name in name_list]
+        ):
+            first_name = ' '.join(name_list[2:])
+            last_name = ' '.join(name_list[:2])
+        else:
+            first_name = ' '.join(name_list[1:])
+            last_name = name_list[0]
+
+        # check names in out_file
+        
+
+        return first_name, last_name
+
+    def read_pdf(self, patient, first_name, last_name) -> None:
+        """Read pdf report and store data in out_file"""
+        if Path(os.path.join(self.root, patient, 'Results', 'Report.pdf')).is_file():
+            reader = pypdf.PdfReader(os.path.join(self.root, patient, 'Results', 'Report.pdf'))
+        elif Path(os.path.join(self.root, patient, 'results', 'Report.pdf')).is_file():
+            reader = pypdf.PdfReader(os.path.join(self.root, patient, 'results', 'Report.pdf'))
+        elif Path(os.path.join(self.root, patient, 'Results', 'Bericht.pdf')).is_file():
+            reader = pypdf.PdfReader(os.path.join(self.root, patient, 'Results', 'Bericht.pdf'))
+        elif Path(os.path.join(self.root, patient, 'results', 'Bericht.pdf')).is_file():
+            reader = pypdf.PdfReader(os.path.join(self.root, patient, 'results', 'Bericht.pdf'))
+        else:
+            logger.info(f'No Report.pdf found for patient {patient}, skipping...')
+            return
+
+        text = reader.pages[0].extract_text().split(sep='\n')
